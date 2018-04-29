8496df26d919e076eb21144f19dddccdcee6aef2
Kevin Clough <kevinclough@gmail.com>
2018-04-29 19:51:25 -0400

    first commit

---
+++ mtrigger-web-poc/Assets/BluetoothWebEvents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class BluetoothWebEvents : MonoBehaviour {
+
+	// NOTE cannot start bluetooth events from Unity due to web security standards
+	// All bluetooth API must be started from a user action like a button click on a dom element.
+
+	[DllImport("__Internal")]
+	private static extern void startButtonClick();
+
+	[DllImport("__Internal")]
+	private static extern void stopButtonClick();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onStartButtonClick() {
+		startButtonClick ();
+	}
+
+	public void onStopButtonClick() {
+		stopButtonClick ();
+	}
+}
---
+++ mtrigger-web-poc/Assets/TextObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextObject : MonoBehaviour {
+
+	public Text channel1TextObject;
+	public Text channel2TextObject;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateChannel1Text (string text) {
+		channel1TextObject.text = "CH 1: " + text;
+	}
+
+	public void updateChannel2Text (string text) {
+		channel2TextObject.text = "CH 2: " + text;
+	}
+}
